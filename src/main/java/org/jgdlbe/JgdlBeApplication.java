2d21f53b69b8f6856fa605105dbae9843ab87be0
박지훈 <wlgns679@gmail.com>
2025-08-29 11:20:03 +0900

    feat:swagger 설정 추가 및 exampl

--- src/main/java/org/jgdlbe/JgdlBeApplication.java
+++ src/main/java/org/jgdlbe/JgdlBeApplication.java
@@ -2,8 +2,10 @@ package org.jgdlbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JgdlBeApplication {
 
 	public static void main(String[] args) {
